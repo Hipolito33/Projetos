573e629241971be9388c3c60b5cdea9ed73e13da
Arthur Hipólito <hipolitoarthur3622@gmail.com>
2026-02-02 13:23:33 -0300

    feat: implementa colisão com canos, placar e reinício do jogo do projeto Flappy-Bird

--- Projetos HTML5, CSS3 & JavaScript/Flappy-Bird/script.js
+++ Projetos HTML5, CSS3 & JavaScript/Flappy-Bird/script.js
@@ -14,64 +14,97 @@ var chao = new Image() // Chão.
 chao.src = 'images/chao.png'
 
 // Variáveis: 
+var gameOver = false
 var eec = 100  // Espaço entre os canos em pixels.
 var constant  // Será usada pra soma depois.
 var bX = 33  // Posição X do Bird.
 var bY = 200  // Posição Y do Bird.
-var gravidade = 0.9  // Gravidade para suavizar os movimentos do Bird.
+var gravidade = 0.7  // Gravidade para suavizar os movimentos do Bird.
 var score = 0  // Guarda o placar do Jogo.
 var cano = []
 
 cano[0] = {
-    x : canvas.width,
-    y : 0
+    x: canvas.width,
+    y: 0
 }
 
 // Carregando Sons:
 var fly = new Audio()
 fly.src = 'sounds/fly.mp3'
-var score = new Audio()
-score.src = 'sounds/score.mp3'
+var Somscore = new Audio()
+Somscore.src = 'sounds/score.mp3'
 
 //Captura de tecla:
 document.addEventListener('keydown', voa); //Quando for pressionada uma tecla, chama a function voa
 
-function voa(){
-    bY = bY-26 
+function voa() {
+    bY = bY - 26
     fly.play()
 }
 
 
-function jogo(){
+function jogo() {
+
+
+    if (gameOver) return
     // fundo do jogo
-    ctx.drawImage(bg,0,0) // Background do jogo com y e x em 0
+    ctx.drawImage(bg, 0, 0) // Background do jogo com y e x em 0
 
 
     // Criando canos
-    for(let i = 0; i < cano.length; i++){ // cano.length(tamanho do cano)
+    for (let i = 0; i < cano.length; i++) { // cano.length(tamanho do cano)
         constant = canocima.height + eec // Altura do cano de baixo vai ser a altura do cano de cima + o espaço entre os canos(100px)
         ctx.drawImage(canocima, cano[i].x, cano[i].y) // Configurando cano de cima
-        ctx.drawImage(canobaixo, cano[i].x, cano[i].y+constant) // Configurando cano de baixo
-        cano[i].x = cano[i].x -1 // Movimentação do cano
+        ctx.drawImage(canobaixo, cano[i].x, cano[i].y + constant) // Configurando cano de baixo
+        cano[i].x = cano[i].x - 1 // Movimentação do cano
 
-        if(cano[i].x == 100){ //Se o cano chagar a 125px da tela(quase no final)
-            cano.push({ //add mais um elemento no array
+        if (cano[i].x == 100) { //Se o cano chagar a 125px da tela(quase no final)
+            cano.push({ // add mais um elemento no array (mais um cano)
                 x: canvas.width, //começa no estremo direito da tela
-                y: Math.floor(Math.random()*canocima.height)-canocima.height //novo cano vindo de forma aleatória
+                y: Math.floor(Math.random() * canocima.height) - canocima.height // novo cano vindo de forma aleatória
             })
         }
-    } 
-        
+
+        if (
+            (
+                bX + bird.width >= cano[i].x &&
+                bX <= cano[i].x + canocima.width &&
+                (
+                    bY <= cano[i].y + canocima.height ||
+                    bY + bird.height >= cano[i].y + constant
+                )
+            )
+            || bY + bird.height >= canvas.height - chao.height
+        ) {
+            gameOver = true // para o jogo
+            setTimeout(() => { //espera um tempo
+                location.reload() // da reload na pág uma única vez
+            }, 800)
+        }
+
+        if (cano[i].x + canocima.width < bX && !cano[i].pontuado) {
+    score++
+    Somscore.play()
+    cano[i].pontuado = true
+}
+    }
+
+
 
 
     //Desenhando o chão
-    ctx.drawImage(chao,0, canvas.height - chao.height)
+    ctx.drawImage(chao, 0, canvas.height - chao.height)
 
     //Desenhando Bird
     ctx.drawImage(bird, bX, bY)
     bY += gravidade
 
+    //Criando Placar
+    ctx.fillStyle = "#000"
+    ctx.font = "20px Veradana"
+    ctx.fillText('Placar: '+ score, 10, canvas.height -20)
+
     requestAnimationFrame(jogo)  //faz um loop infinito para animação de cenário
 }
 
-jogo()
+jogo();
