bab8e62c2544af7fe2fa3adc332312f742eea5af
Arthur Hipólito <hipolitoarthur3622@gmail.com>
2026-02-03 10:21:12 -0300

    Carregados os Cards (imagens) do Projeto Jogo da Memória

---
+++ Projetos HTML5, CSS3 & JavaScript/Jogo da Memória/app.js
@@ -0,0 +1,53 @@
+addEventListener('DOMContentLoaded', () => { // Inicia sem precisar esperar o carregamento do css ou das imagens(cards).
+    const cardArray = [ // Carregando os cards em um array de objetos.
+        {
+            nome : 'ganhou',
+            img : 'imagens/ganhou.png'
+        },
+        {
+            nome : 'ganhou',
+            img : 'imagens/ganhou.png'
+        },
+        {
+            nome : 'direita',
+            img : 'imagens/direita.png'
+        },
+        {
+            nome : 'direita',
+            img : 'imagens/direita.png'
+        },
+        {
+            nome : 'tras',
+            img : 'imagens/tras.png'
+        },
+        {
+            nome : 'tras',
+            img : 'imagens/tras.png'
+        },
+        {
+            nome : 'correndo', 
+            img : 'imagens/correndo.png'
+        },
+        {
+            nome : 'correndo', 
+            img : 'imagens/correndo.png'
+        },
+        {
+            nome : 'pulo',
+            img : 'imagens/pulo.png'
+        },
+        {
+            nome : 'pulo',
+            img : 'imagens/pulo.png'
+        },
+        {
+            nome : 'esquerda',
+            img : 'imagens/esquerda.png'
+        },
+        {
+            nome : 'esquerda',
+            img : 'imagens/esquerda.png'
+        }
+
+    ]        
+})
